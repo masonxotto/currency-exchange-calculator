21b37372dcbe58fb2e320e4b72de932ead01fbd4
masonxotto <masonxotto@gmail.com>
2022-09-27 13:31:30 -0500

    added files

---
+++ index.js
@@ -0,0 +1,32 @@
+//javascript file
+let requestURL = "https://api.exchangerate.host/latest";
+let request = new XMLHttpRequest();
+let conversion = document.getElementById("response");
+/*
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+*/
+
+
+request.onload = function() {
+    let response = request.response;
+    //console.log(response); 
+    conversion.innerHTML = "Conversion: " + document.getElementById("ex1").value + " to " + document.getElementById("ex2").value + " is " + response.result;
+ }              
+
+
+function convert() {
+    let from = "from=" + document.getElementById("ex1").value;
+    let to = "to=" + document.getElementById("ex2").value;
+    //console.log(from);
+    //console.log(to);
+    
+    let newRequest = 'https://api.exchangerate.host/convert?places=2&' + from + "&" + to + "&amount=1";
+    //console.log(newRequest);
+    request.open('GET', newRequest);
+    request.responseType = 'json';
+    request.send();
+    let response = request.response;
+    
+}
